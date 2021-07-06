1285ed122e3c977850db9ca8caa703da1fd84a69
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-06 14:30:22 +0300

    feat: add modal window

--- src/components/Mainsection/Mainsection_content/Mainsection_content_searchbar/MainSectionContentSearchBar.tsx
+++ src/components/Mainsection/Mainsection_content/Mainsection_content_searchbar/MainSectionContentSearchBar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 type MainSectionContentSearchbarProps = React.HTMLAttributes<HTMLElement>;
 
 export const MainSectionContentSearchbar: React.FC<MainSectionContentSearchbarProps> =
   () => {
+    const [isModalHidden, setIsModalHidden] = useState(false);
     return (
       <div className="searchForm row">
         <div className="col-sm-14">
@@ -26,8 +27,10 @@ export const MainSectionContentSearchbar: React.FC<MainSectionContentSearchbarPr
           </form>
         </div>
         <div className="options col-sm-10">
-          <a
-            href="index.html"
+          <button
+            onClick={() => {
+              setIsModalHidden(!isModalHidden);
+            }}
             title="Add new item"
             className="newItem"
             data-toggle="modal"
@@ -35,8 +38,56 @@ export const MainSectionContentSearchbar: React.FC<MainSectionContentSearchbarPr
           >
             <i className="icon-plus-small"></i>
             <span>New Item</span>
-          </a>
-
+          </button>
+          <div
+            className={isModalHidden ? 'modal fade in' : 'modal fade'}
+            id="myModal"
+            tabIndex={-1}
+            role="dialog"
+            aria-labelledby="myModalLabel"
+            aria-hidden={isModalHidden}
+            style={isModalHidden ? { display: 'block' } : { display: 'none' }}
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">×</span>
+                  </button>
+                  <h4 className="modal-title" id="myModalLabel">
+                    Add new item
+                  </h4>
+                </div>
+                <div className="modal-body">
+                  <textarea
+                    className="form-control"
+                    rows={3}
+                    defaultValue={''}
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    data-dismiss="modal"
+                    onClick={() => {
+                      setIsModalHidden(!isModalHidden);
+                    }}
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <a href="index.html" className="btn btn-sm btn-option">
             <i className="icon-sliders"></i>
           </a>
