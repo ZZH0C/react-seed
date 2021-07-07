5bd7954aeea692f4cd7f322a234f11abbb50d87c
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-07 14:12:03 +0300

    refactor: add mock data

---
+++ src/components/mockProps.js
@@ -0,0 +1,21 @@
+export const testProps_subItemOn = {
+  href: 'index.html',
+  name: 'subitem 1',
+  active: true,
+};
+
+export const testProps_subItemOff = {
+  href: 'index.html',
+  name: 'subitem 0',
+  active: false,
+};
+
+export const testPropsNavBarOff = {
+  isActive: false,
+  content: 'Test Bar 0',
+};
+
+export const testPropsNavBarOn = {
+  isActive: true,
+  content: 'Test Bar 1',
+};
