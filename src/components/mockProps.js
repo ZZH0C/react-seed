e33a200b13e1e0bd4e4be08587cf1079b900fa65
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-12 12:35:43 +0300

    refactor: change name

--- src/components/mockProps.js
+++ src/components/mockProps.js
@@ -1,12 +1,12 @@
 export const testProps_subItemOn = {
   href: 'index.html',
-  name: 'subitem 1',
+  name: 'subitem',
   active: true,
 };
 
 export const testProps_subItemOff = {
   href: 'index.html',
-  name: 'subitem 0',
+  name: 'subitem',
   active: false,
 };
 
