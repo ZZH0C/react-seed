68ca0cd80ffc4ae71f6244e68620255ed4211211
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-12 14:50:02 +0300

    refactor: remove useless hook

--- src/components/HeaderUser/HeaderUser.tsx
+++ src/components/HeaderUser/HeaderUser.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { UserContext } from '../../pages/HomePage/HomePage';
 
 interface HeaderUserProps extends React.HTMLAttributes<HTMLElement> {
@@ -6,37 +6,29 @@ interface HeaderUserProps extends React.HTMLAttributes<HTMLElement> {
   alt?: string;
 }
 
+const emptyProfilePictureSrc =
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+
 export const HeaderUser: React.FC<HeaderUserProps> = ({ children }) => {
   const userData = useContext(UserContext);
-  const emptyProfilePictureSrc =
-    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
 
-  const [profileData, setProfileSrc] = useState({
+  const profile = {
     name: '',
     iconSrc: emptyProfilePictureSrc,
-  });
-
-  useEffect(() => {
-    if (!userData) {
-      setProfileSrc({
-        name: '',
-        iconSrc: emptyProfilePictureSrc,
-      });
+  };
+  if (userData) {
+    if ('profileObj' in userData) {
+      profile.name = userData.profileObj.name;
+      profile.iconSrc = userData.profileObj.imageUrl;
     }
-    if (userData && 'profileObj' in userData) {
-      setProfileSrc({
-        iconSrc: userData.profileObj.imageUrl,
-        name: userData.profileObj.name,
-      });
-    }
-  }, [userData]);
+  }
 
   return (
     <div className="header-comp pull-right">
       {children}
       <a href="./index.html" className="profile">
-        <span>{profileData.name}</span>
-        <img alt={'userPick'} src={profileData.iconSrc} />
+        <span>{profile.name}</span>
+        <img alt={'userPick'} src={profile.iconSrc} />
       </a>
       <a href="./index.html" className="btn btn-xs btn-header">
         <i className="headerIcon icon-search" />
