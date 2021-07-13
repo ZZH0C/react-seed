0f40cdad3a25548c1c31b8d3f5b33baf49f5dd91
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-13 16:55:48 +0300

    refactor: add brackets

--- src/hooks/useGetMessages.tsx
+++ src/hooks/useGetMessages.tsx
@@ -5,8 +5,12 @@ export const useGetMessages = () => {
   const [state, setState] = useState<any>([]);
 
   const setMessageList = (token: string | null) => {
-    if (token) loadMessages(token).then((r) => setState(r));
-    if (!token) setState([]);
+    if (token) {
+      loadMessages(token).then((r) => setState(r));
+    }
+    if (!token) {
+      setState([]);
+    }
   };
   return { setMessageList, state };
 };
