ff9acaf7a550292289c2aa38107b846dd1366af0
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-13 16:23:03 +0300

    feat: add hook for messages UI

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,47 +1,8 @@
-import React, { useContext, useEffect } from 'react';
-import { UserContext } from '../../pages/HomePage/HomePage';
-import { useGetMessages } from '../../hooks/useGetMessages';
-import { MessageItem } from '../MessageItem/MessageItem';
-import { GoogleMessage } from '../../models/GoogleMessage';
+import React from 'react';
+import { useCreateMessagesUi } from '../../hooks/useCreateMessagesUi';
 type MainSectionProps = React.HTMLAttributes<HTMLElement>;
 
 export const MainSection: React.FC<MainSectionProps> = () => {
-  const userData = useContext(UserContext);
-  const { setMessageList, state } = useGetMessages();
-
-  const messages: JSX.Element[] = [];
-
-  useEffect(() => {
-    if (userData && 'accessToken' in userData) {
-      const token = userData.accessToken;
-      setMessageList(token);
-    }
-    if (!userData) {
-      setMessageList(null);
-    }
-  }, [userData]);
-
-  if (state.length > 0) {
-    state.forEach((e: GoogleMessage) => {
-      const props = {
-        from: '',
-        snippet: e.value.snippet,
-        title: '',
-      };
-      e.value.payload.headers.forEach((e: { name: string; value: string }) => {
-        if (e.name === 'From') props.from = e.value;
-        if (e.name === 'Subject') props.title = e.value;
-      });
-      messages.push(
-        <MessageItem
-          key={Math.random()}
-          fromWho={props.from}
-          messageSnippet={props.snippet}
-          messageTitle={props.title}
-        />,
-      );
-    });
-  }
-
+  const messages = useCreateMessagesUi();
   return <section className="mainSection">{messages}</section>;
 };
