68c84c3b023c2e8a32533dca8a803000372bbbc0
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-20 09:34:51 +0300

    refactor: add return types and separate 1 function into 2 to be more readable

--- src/api/userMessages/userMessages.tsx
+++ src/api/userMessages/userMessages.tsx
@@ -1,4 +1,11 @@
 import axios from 'axios';
+import { SetStateAction } from 'react';
+import { GoogleMessage } from '../../models/GoogleMessage';
+
+interface GoogleFilter {
+  token: string;
+  category: string;
+}
 
 const getMessage = async (id: string, token: string) => {
   const url = `https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}`;
@@ -13,13 +20,13 @@ const getMessage = async (id: string, token: string) => {
   }
 };
 
-const getMessageList = async (token: string, category: string) => {
+const getMessageList = async (filter: GoogleFilter) => {
   const url = `https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=3`;
   try {
     const response = await axios.get(url, {
-      params: { q: category },
+      params: { q: filter.category },
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${filter.token}`,
       },
     });
     return response.data;
@@ -29,17 +36,34 @@ const getMessageList = async (token: string, category: string) => {
   }
 };
 
-export const loadMessages = async (token: string, category: string) => {
-  const idList = await getMessageList(token, category);
-
-  if (idList && idList.messages) {
-    const messageList = idList.messages.map(async (e: { id: string }) => {
-      return await getMessage(e.id, token);
-    });
-    return await Promise.allSettled(messageList);
-  } else return [];
+const getMessagesArray = async (idArr: { id: string }[], token: string) => {
+  return idArr.map((e: { id: string }) => {
+    return getMessage(e.id, token);
+  });
 };
 
-export const loadOneMessage = async (id: string, token: string) => {
+export const loadMessages = async (
+  token: string,
+  category: string,
+): Promise<
+  Array<
+    PromiseSettledResult<
+      Promise<any> extends PromiseLike<infer U> ? U : Promise<any>
+    >
+  >
+> => {
+  const idList = await getMessageList({ token: token, category: category });
+  if (idList && idList.messages) {
+    const messageList = await getMessagesArray(idList.messages, token);
+    return Promise.allSettled(messageList);
+  } else {
+    return [];
+  }
+};
+
+export const loadOneMessage = async (
+  id: string,
+  token: string,
+): Promise<SetStateAction<GoogleMessage | undefined>> => {
   return await getMessage(id, token);
 };
