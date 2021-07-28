aab45defcb423f7fa42638829396a9b4ec460010
QULIX\Lutsevichsl <zzh0czz@gmail.com>
2021-07-28 14:06:16 +0300

    feat: add hiding searchbar and pagination on unlogined

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useCreateMessagesUi } from '../../hooks/useCreateMessagesUi/useCreateMessagesUi';
 import { SearchBar } from '../SearchBar/SearchBar';
+import styles from './MainSection.module.scss';
+import classNames from 'classnames';
+import { UserContext } from '../../pages/HomePage/HomePage';
 type MainSectionProps = React.HTMLAttributes<HTMLElement>;
 
 export const MainSection: React.FC<MainSectionProps> = () => {
+  const userData = useContext(UserContext);
+  const [hiddenStyle, setHiddenStyle] = useState(styles.hidden);
+  useEffect(() => {
+    if (userData) {
+      setHiddenStyle('');
+    } else {
+      setHiddenStyle(styles.hidden);
+    }
+  }, [userData]);
   const messages = useCreateMessagesUi();
   return (
-    <section className="mainSection">
+    <section className={classNames(hiddenStyle, 'mainSection')}>
       <SearchBar />
       {messages}
     </section>
